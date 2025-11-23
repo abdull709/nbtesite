4f0e69f4d18cbac1f4fef183fc8af029d4678d6d
abdull709 <66621497+abdull709@users.noreply.github.com>
2025-11-23 17:41:17 +0100

    update

--- nbtelog/models.py
+++ nbtelog/models.py
@@ -89,7 +89,7 @@ class Program(models.Model):
 class Document(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
-    file = models.FileField(upload_to='downloads/')
+    file = models.FileField(upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
